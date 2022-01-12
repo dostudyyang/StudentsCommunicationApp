53eb5cf9591f919834c6a729873688337f096040
Vincent-kipngeno <kipngenovi2001@gmail.com>
2022-01-12 19:39:29 +0300

    Create RegistrationActivity with its layout file

---
+++ app/src/main/java/com/example/college_students_communication_app/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.college_students_communication_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
