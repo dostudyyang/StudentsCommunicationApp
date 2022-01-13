0e17a8beafd77ec19a0415aff78b8f030fe03187
Vincent-kipngeno <kipngenovi2001@gmail.com>
2022-01-13 12:23:39 +0300

    Create validate inputs function used to validate the email and password inputs before login

--- app/src/main/java/com/example/college_students_communication_app/LoginActivity.java
+++ app/src/main/java/com/example/college_students_communication_app/LoginActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.college_students_communication_app.databinding.ActivityLoginBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -51,4 +54,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
 
     }
+
+    public boolean validateInputs(String email, String password){
+        if (TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            binding.emailEditText.setError("Please enter email...");
+            return false;
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            binding.passwordEditText.setError("Please enter password...");
+            return  false;
+        }
+        else {
+            return true;
+        }
+    }
+
 }
