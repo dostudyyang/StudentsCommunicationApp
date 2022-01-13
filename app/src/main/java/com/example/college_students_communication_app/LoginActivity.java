9d468234620a097acd09e2eff8b9c1da3c4d2a31
Vincent-kipngeno <kipngenovi2001@gmail.com>
2022-01-13 12:04:05 +0300

    Initialize FirebaseAuth and UsersRef database reference, then check whether user is logged in, in the OnStart method

--- app/src/main/java/com/example/college_students_communication_app/LoginActivity.java
+++ app/src/main/java/com/example/college_students_communication_app/LoginActivity.java
@@ -2,14 +2,24 @@ package com.example.college_students_communication_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.college_students_communication_app.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
 
 
     @Override
@@ -17,8 +27,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-
-
         setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null)
+        {
+            //SendUserJoinGroupActivity();
+        }
     }
 }
