f542ed0f0f8656ff3f21fcf87f0775e153f8a8ff
Vincent-kipngeno <kipngenovi2001@gmail.com>
2022-01-13 16:42:48 +0300

    Bind layout views to RegistrationActivity

--- app/src/main/java/com/example/college_students_communication_app/RegistrationActivity.java
+++ app/src/main/java/com/example/college_students_communication_app/RegistrationActivity.java
@@ -2,13 +2,29 @@ package com.example.college_students_communication_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.college_students_communication_app.databinding.ActivityLoginBinding;
+import com.example.college_students_communication_app.databinding.ActivityRegistrationBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private ActivityRegistrationBinding binding;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRootRef;
+
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
