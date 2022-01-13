edc868e2a735a247c40efdf140c91585932dfdc0
Vincent-kipngeno <kipngenovi2001@gmail.com>
2022-01-14 00:13:25 +0300

    Update the chats list in OnStart function incase a new message is posted

--- app/src/main/java/com/example/college_students_communication_app/GroupChatActivity.java
+++ app/src/main/java/com/example/college_students_communication_app/GroupChatActivity.java
@@ -1,5 +1,7 @@
 package com.example.college_students_communication_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,17 +15,24 @@ import com.example.college_students_communication_app.databinding.ActivityGroupC
 import com.example.college_students_communication_app.databinding.ActivityJoinGroupBinding;
 import com.example.college_students_communication_app.models.Chat;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupChatActivity extends AppCompatActivity {
+public class GroupChatActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityGroupChatBinding binding;
 
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
+    private String messageSenderID;
+    private String groupCode;
 
     private final List<Chat> chats = new ArrayList<>();
     private LinearLayoutManager linearLayoutManager;
@@ -36,6 +45,55 @@ public class GroupChatActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        mAuth = FirebaseAuth.getInstance();
+        messageSenderID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
 
+
+        groupCode = getIntent().getExtras().get("groupCode").toString();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        actionBar.setTitle(groupCode);
+
+        groupMessageAdapter = new GroupMessageAdapter(chats);
+        linearLayoutManager = new LinearLayoutManager(this);
+        binding.groupChatMessages.setLayoutManager(linearLayoutManager);
+        binding.groupChatMessages.setAdapter(groupMessageAdapter);
+
+        binding.sendMessageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == binding.sendMessageButton){
+
+        }
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        chats.clear();
+        RootRef.child("Chats").child(groupCode).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Chat chat = dataSnapshot.getValue(Chat.class);
+
+                //chats.add(chat);
+
+                groupMessageAdapter.notifyDataSetChanged();
+
+                binding.groupChatMessages.smoothScrollToPosition(binding.groupChatMessages.getAdapter().getItemCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
