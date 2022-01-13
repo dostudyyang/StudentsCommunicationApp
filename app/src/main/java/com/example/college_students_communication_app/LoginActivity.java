f0959913ae918cadbb8362a0e5ab153c6a10593d
Vincent-kipngeno <kipngenovi2001@gmail.com>
2022-01-13 11:58:13 +0300

    Enable view binding and apply it in LoginActivity

--- app/src/main/java/com/example/college_students_communication_app/LoginActivity.java
+++ app/src/main/java/com/example/college_students_communication_app/LoginActivity.java
@@ -3,12 +3,22 @@ package com.example.college_students_communication_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.college_students_communication_app.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+
+        setContentView(view);
     }
 }
